93247a4fa44e35dda1ad8ad3c2fbe833288c5fd8
Sundaresan <susurend@186590dd90d1.ant.amazon.com>
2017-07-26 15:50:58 +0530

    Autowired annotation with required set to false

--- com/tutorialspoint/TextEditor.java
+++ com/tutorialspoint/TextEditor.java
@@ -7,15 +7,17 @@ public class TextEditor {
 
 
     private SpellChecker spellChecker;
+    @Autowired(required = false)//if set to false, then the value will not be set.
     private String name;
 
     @Autowired
-    public TextEditor(SpellChecker spellChecker,String name) {
+    public TextEditor(SpellChecker spellChecker) {
         //if the name is not specified in the constructor-arg, then on autowiring exception will be thrown.
         System.out.println("Inside text editor constructor");
         this.spellChecker = spellChecker;
-        this.name = name;
     }
+
+
 
     @Override
     public String toString() {
