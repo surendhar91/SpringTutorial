c5b40b52f792f820c73632c5fcb00b923553b0c6
Sundaresan <susurend@186590dd90d1.ant.amazon.com>
2017-07-26 14:20:46 +0530

    Inside Bean

--- com/tutorialspoint/InitHelloWorld.java
+++ com/tutorialspoint/InitHelloWorld.java
@@ -9,14 +9,14 @@ public class InitHelloWorld implements BeanPostProcessor{
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         //before init method of bean getting called.
 //        ((HelloWorld)bean).getMessage1();
-        System.out.println("Post process before initialization of bean "+beanName);
+//        System.out.println("Post process before initialization of bean "+beanName);
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         //after init method of bean getting called.
-        System.out.println("Post process after initialization of bean "+beanName);
+//        System.out.println("Post process after initialization of bean "+beanName);
         return bean;
     }
 }
--- com/tutorialspoint/TextEditor.java
+++ com/tutorialspoint/TextEditor.java
@@ -1,5 +1,7 @@
 package com.tutorialspoint;
 
+import javax.xml.soap.Text;
+
 public class TextEditor {
     private SpellChecker spellChecker;
 
@@ -7,6 +9,9 @@ public class TextEditor {
         return spellChecker;
     }
 
+    public TextEditor(){
+        System.out.println("Inside TextEditor constructor.");
+    }
     public void setSpellChecker(SpellChecker spellChecker) {
         this.spellChecker = spellChecker;
     }
