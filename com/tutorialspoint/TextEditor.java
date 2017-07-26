d420d28f8034f97d434040a81f8f10532babea6e
Sundaresan <susurend@186590dd90d1.ant.amazon.com>
2017-07-26 16:19:13 +0530

    qualifier on set property

--- com/tutorialspoint/TextEditor.java
+++ com/tutorialspoint/TextEditor.java
@@ -9,14 +9,21 @@ public class TextEditor {
     @Autowired(required = false)//if set to false, then the value will not be set.
     private String name;
 
-    @Autowired
-    public TextEditor(@Qualifier(value = "spellChecker1")SpellChecker spellChecker) {
+    public TextEditor() {
         //@Qualifier(value = "spellChecker1") before the constructor
         //if the name is not specified in the constructor-arg, then on autowiring exception will be thrown.
         System.out.println("Inside text editor constructor");
-        this.spellChecker = spellChecker;
     }
 
+    public SpellChecker getSpellChecker() {
+        return spellChecker;
+    }
+
+    @Autowired
+    @Qualifier(value = "spellChecker2")
+    public void setSpellChecker(SpellChecker spellChecker) {
+        this.spellChecker = spellChecker;
+    }
 
     @Override
     public String toString() {
