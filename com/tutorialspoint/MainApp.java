e00116a380fe623c31bad09046c1f4e75ac6a6ec
Sundaresan <susurend@186590dd90d1.ant.amazon.com>
2017-07-26 13:23:02 +0530

    shutdown hook registered

--- com/tutorialspoint/MainApp.java
+++ com/tutorialspoint/MainApp.java
@@ -1,20 +1,14 @@
 package com.tutorialspoint;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-
-import java.beans.beancontext.BeanContext;
 
 public class MainApp {
     public static void main(String[] args) {
         //Inversion of control
 
         //as and when the beans.xml is loaded, all the objects will be instantiated.
-        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
         //only when you call the getBean method, the object will be instantiated.
 //        Resource resource = new ClassPathResource("Beans.xml");
@@ -24,7 +18,9 @@ public class MainApp {
         instance1.setMessage("Instance 1");
         instance1.getMessage();
 
-        HelloWorld instance2 = (HelloWorld)context.getBean("helloWorld");
+        HelloWorld instance2 = (HelloWorld) context.getBean("helloWorld");
         instance2.getMessage();
+        context.registerShutdownHook();
+        //register the shutdown hook with jvm, so that the resources get destroyed.
     }
 }
