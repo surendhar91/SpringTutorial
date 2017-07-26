ed06002678d7b2e4dc811ac747727213e53363ee
Sundaresan <susurend@186590dd90d1.ant.amazon.com>
2017-07-26 15:12:07 +0530

    Required property annotation added

--- com/tutorialspoint/TextEditor.java
+++ com/tutorialspoint/TextEditor.java
@@ -1,14 +1,31 @@
 package com.tutorialspoint;
 
-import javax.xml.soap.Text;
+import org.springframework.beans.factory.annotation.Required;
 
 public class TextEditor {
+    public SpellChecker getSpellChecker() {
+        return spellChecker;
+    }
+
+    @Required
+    public void setSpellChecker(SpellChecker spellChecker) {
+        this.spellChecker = spellChecker;
+    }
+
     private SpellChecker spellChecker;
     private String name;
 
-    public TextEditor(SpellChecker spellChecker, String name){
+    public TextEditor() {
         System.out.println("Inside text editor constructor");
-        this.spellChecker = spellChecker;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Required
+    //applies to bean property setter methods, must be populated in xml configuration file
+    public void setName(String name) {
         this.name = name;
     }
 
