0a4f96f03ce97c9b4f1a74a8414e911a02d70c24
Sundaresan <susurend@186590dd90d1.ant.amazon.com>
2017-07-27 13:28:06 +0530

    added one more advice

--- com/tutorialspoint/aspect/LoggingAspect.java
+++ com/tutorialspoint/aspect/LoggingAspect.java
@@ -7,8 +7,16 @@ import org.aspectj.lang.annotation.Before;
 public class LoggingAspect {
     //Cross cutting concerns are the secondary requirement / system wide requirements for our application.
     // Example: logging, security, data transfer, authentication/authorization of method calls, transaction management etc..
-    @Before("execution(public String com.tutorialspoint.aspect.Circle.getName())")
+    @Before("execution(public * get*())")
+    //(*) - one ore more arguments
+    //(..) - zero or more arguments
+    //() - zero arguments
     public void LoggingAdvice(){//standard terminology
         System.out.println("Advice run. Get Method called.");
+    }
+
+    @Before("execution(public * get*())")
+    public void secondAdvice(){
+        System.out.println("Second advice running..");
     }
 }
